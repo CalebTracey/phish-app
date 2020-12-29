3b7eaba73fa85c4ea3e4cffbf649c431a80945bc
Caleb Tracey <caleb.tracey@gmail.com>
2020-12-29 14:50:09 -0500

    Added basic router functionality

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,44 +1,59 @@
 import ShowList from '../ShowList'
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from '../axios';
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            d: this.props.data,
-            choice: "Pick an option"
+            //d: this.props.data,
+            choice: null,
+            isToggleOn: false,
+            //data: [],
         }
+        this.openShowList = this.openShowList.bind(this);
+    }
+
+    getShowData() {
+        // Make HTTP reques with Axios
+        axios.get('years/2003')
+            .then((res) => {
+                // Set state with result
+                this.setState({
+                    //data: res.data,
+                    choice: <ShowList shows={res.data.data} />
+                });
+            });
     }
 
     openShowList() {
-        this.setState({
-            choice: <div className="App">
-                <ShowList
-                    shows={this.state.d}
-                />
-            </div>
-        })
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+        if (this.isToggleOn !== false) {
+            this.getShowData()
+        } else { this.setState({ choice: null }) };
     }
+
     render() {
         return (
-            <div>
-                <h2>Dashboard</h2>
-                <button onClick={() => {
-                    this.openShowList()
-                }}>
-                    Show List
-                  </button>
+            <div className="dash-bar">
+                <div>
+                    <button onClick={
+                        () => { this.openShowList() }}> Show List
+                    </button>
+                </div>
+
                 <div>
                     {this.state.choice}
                 </div>
             </div>
         );
     }
-
 }
-Dashboard.propTypes = {
-    data: PropTypes.array,
-};
+// Dashboard.propTypes = {
+//     data: PropTypes.array,
+// };
 
 export default Dashboard
--- src/PhishApp.js
+++ src/PhishApp.js
@@ -1,12 +1,11 @@
 
-import './App.css';
+import './style.css';
+import './index.css';
 import response from './data'
 import React, { Component } from 'react';
+import ContentPane from './ContentPane';
 import ShowList from './ShowList';
 import axios from './axios';
-import Home from './Dashboard/Home';
-import About from './Dashboard/About';
-import Dashboard from './Dashboard/Dashboard';
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,59 +18,79 @@ class PhishApp extends Component {
     super(props);
 
     this.state = {
-      data: response.data
+      data: []
     }
   }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <div>
-            <Link to="/">Home</Link>
-          </div>
-          <div>
-            <Link to="/about">About</Link>
-          </div>
-          <div>
-            <Link to="/dashboard">Dashboard</Link>
-          </div>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard data={this.state.data} />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    )
-  }
-
   // componentDidMount() {
   //   // Make HTTP reques with Axios
-  //   axios.get('/years/2003')
+  //   axios.get('years/2003')
   //       .then((res) => {
   //           // Set state with result
   //           this.setState({
   //               data: res.data
   //           });
   //       });
-
+  //     }
+  render() {
+    return (
+      <Router>
+        <div className="wrapper">
+          <div className="nav-bar">
+            <div className="nav-item">
+              <Link to="home">Home</Link>
+            </div>
+            <div className="nav-item">
+              <Link to="about">About</Link>
+            </div>
+          </div>
+          <div className="side-and-content">
+            <div className="side-bar">
+              <div className="nav-item">
+                <Link to="search">Search</Link></div>
+              <div className="nav-item">
+                <Link to="years">Years</Link></div>
+              <div className="nav-item">
+                <Link to="tours">Tours</Link></div>
+              <div className="nav-item">
+                <Link to="songs">Songs</Link></div>
+            </div>
+            <div className="content-pane">
+              <Switch>
+                <Route exact path="/home">
+                  <ContentPane choiceString={"home"} />
+                </Route>
+                <Route path="/about">
+                  <ContentPane choiceString={"about"} />
+                </Route>
+                <Route path="search">
+                  <ContentPane choiceString={"search"} />
+                </Route>
+                <Route path="years">
+                  <ContentPane choiceString={"years"} />
+                </Route>
+                <Route path="tours">
+                  <ContentPane choiceString={"tours"} />
+                </Route>
+                <Route path="songs">
+                  <ContentPane choiceString={"songs"} />
+                </Route>
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
+    )
+  }
   // render() {
   //   return (
 
-      // <div>
-      //   <div className="App">
-      //     <ShowList
-      //       shows={this.state.data}
-      //     />
-      //   </div>
-      // </div>
+  // <div>
+  //   <div className="App">
+  //     <ShowList
+  //       shows={this.state.data}
+  //     />
+  //   </div>
+  // </div>
   //   );
   // }
 }

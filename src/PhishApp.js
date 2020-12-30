5816a85572ee6a9984039532b48a012f6350125a
dtracey <1Passw0rd>
2020-12-29 22:01:20 -0500

    bootstrap top nav

--- src/PhishApp.js
+++ src/PhishApp.js
@@ -1,14 +1,10 @@
 import "./style.css";
 import "./index.css";
-import response from "./data";
 import React, { Component } from "react";
-import ContentPane from "./ContentPane";
-import ShowList from "./ShowList";
-import axios from "./axios";
 import Home from './Header/Home';
 import About from './Header/About';
 import Years from './Navbar/Years';
-import Tours from './Navbar/Tours';
+import TopNavBar from './Navbar/TopNavBar';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class PhishApp extends Component {
@@ -19,29 +15,21 @@ class PhishApp extends Component {
       data: [],
     };
   }
-  // componentDidMount() {
-  //   // Make HTTP reques with Axios
-  //   axios.get('years/2003')
-  //       .then((res) => {
-  //           // Set state with result
-  //           this.setState({
-  //               data: res.data
-  //           });
-  //       });
-  //     }
 
   render() {
     return (
       <Router>
+                  
         <div className="wrapper">
-          <div className="nav-bar">
+          {/* <div className="nav-bar">
             <div className="nav-item">
               <Link to="/">Home</Link>
             </div>
             <div className="nav-item">
               <Link to="/about">About</Link>
             </div>
-          </div>
+          </div> */}
+<TopNavBar/>
           <div className="side-and-content">
             <div className="side-bar">
               <div className="nav-item">

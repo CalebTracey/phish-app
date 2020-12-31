05f4d945cb719b3a5b6e11d13853bafd1a424ab8
Caleb Tracey <caleb.tracey@gmail.com>
2020-12-31 11:07:11 -0500

    Router functionality with ShowDetails

--- src/Components/Show.js
+++ src/Components/Show.js
@@ -3,54 +3,51 @@ import PropTypes from 'prop-types';
 import '../style.css';
 
 
-const Show = ({show}) => {
+class Show extends Component {
 
-    return (
-        show.id
-    )
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         s: this.props.show,
-    //         notesClass: "taper-notesOFF",
-    //     }
-    //     this.showNotes = this.showNotes.bind(this);
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            s: this.props.show,
+            notesClass: "taper-notesOFF",
+        }
+        this.showNotes = this.showNotes.bind(this);
+    }
 
-    // showNotes() {
-    //     if (this.state.notesClass === "taper-notesOFF") {
-    //         this.setState({ notesClass: "taper-notesON" });
-    //     } else {
-    //         this.setState({ notesClass: "taper-notesOFF" });
-    //     }
-    // }
+    showNotes() {
+        if (this.state.notesClass === "taper-notesOFF") {
+            this.setState({ notesClass: "taper-notesON" });
+        } else {
+            this.setState({ notesClass: "taper-notesOFF" });
+        }
+    }
 
-//     render() {
-//         return (
-//             <div className="show"
-//                 onClick={
-//                     () => {
-//                         if (this.state.notesClass === "taper-notesOFF") {
-//                             this.setState({ notesClass: "taper-notesON" });
-//                         } else {
-//                             this.setState({ notesClass: "taper-notesOFF" });
-//                         }
-//                     }
-//                 }>
-//                 <div className="show-row-1">
-//                     {/* <div className="show-field"> {this.state.s.date} </div> */}
-//                     {/* <div className="show-field"> {this.state.s.venue_name} </div> */}
-//                 </div>
-//                 <div className="show-row-2">
-//                     <div className={this.state.notesClass}> {this.state.s.taper_notes} </div>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-// Show.propTypes = {
-//     show: PropTypes.object,
-// };
+    render() {
+        return (
+            <div className="show"
+                onClick={
+                    () => {
+                        if (this.state.notesClass === "taper-notesOFF") {
+                            this.setState({ notesClass: "taper-notesON" });
+                        } else {
+                            this.setState({ notesClass: "taper-notesOFF" });
+                        }
+                    }
+                }>
+                <div className="show-row-1">
+                    <div className="show-field"> {this.state.s.data.date} </div>
+                    <div className="show-field"> {this.state.s.data.venue_name} </div>
+                </div>
+                <div className="show-row-2">
+                    <div className={this.state.notesClass}> {this.state.s.data.taper_notes} </div>
+                </div>
+            </div>
+        );
+    }
 }
+
+Show.propTypes = {
+    show: PropTypes.object,
+};
+
 export default Show

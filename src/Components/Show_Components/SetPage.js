e2aaf0923671cc5588212de9053774e26b022de6
Caleb Tracey <caleb.tracey@gmail.com>
2021-01-02 02:29:45 -0500

    Brute force set/track length conversions

--- src/Components/Show_Components/SetPage.js
+++ src/Components/Show_Components/SetPage.js
@@ -5,6 +5,9 @@ import PropTypes from 'prop-types';
 
 const SetPage = (props) => {
     let { tracks, setKey } = props
+    const hr = 3600000
+    const min = 60000
+    const sec = 1000
 
     let setTracks = tracks.filter((track) => {
         return track.set === setKey;
@@ -14,17 +17,57 @@ const SetPage = (props) => {
         a + b.duration, 0
     )
 
-    return (
-        <div>
-            <SetHeader duration={setLength} setKey={setKey} />
-            <TrackList tracks={setTracks} setKey={setKey} />
-        </div>
-    );
-}
+    function convertLength() {
+        var indexHr = 0, indexMin = 0, indexSec = 0
 
-SetPage.propTypes = {
-    tracks: PropTypes.array,
-    setKey: PropTypes.string,
-}
+        if (setLength > hr) {
+            while (setLength > hr) {
+                setLength = setLength - hr
+                indexHr++
+            } while (setLength > min) {
+                setLength = setLength - min
+                indexMin++
+            } if (indexMin < 10) {
+                indexMin = "0" + indexMin
+            } while (setLength > sec) {
+                setLength = setLength - sec
+                indexSec++
+            } if (indexSec < 10) {
+                indexSec = "0" + indexSec
+            } return (indexHr + ":" + indexMin + ":" + indexSec)
+        } else if (setLength > min) {
+            while (setLength > min) {
+                setLength = setLength - min
+                indexMin++
+            } if (indexMin < 10) {
+                indexMin = "0" + indexMin
+            } while (setLength > sec) {
+                setLength = setLength - sec
+                indexSec++
+            } if (indexSec < 10) {
+                indexSec = "0" + indexSec
+            } return (indexMin + ":" + indexSec)
+        } else if (setLength > sec) {
+            while (setLength > sec) {
+                setLength = setLength - sec
+                indexSec++
+            } if (indexSec < 10) {
+                indexSec = "0" + indexSec
+            }return (indexSec + "seconds")
+        }
+    }
 
-export default SetPage;
+        return (
+            <div>
+                <SetHeader duration={convertLength(setLength)} setKey={setKey} />
+                <TrackList tracks={setTracks} setKey={setKey} />
+            </div>
+        );
+    }
+
+    SetPage.propTypes = {
+        tracks: PropTypes.array,
+        setKey: PropTypes.string,
+    }
+
+    export default SetPage;
--- src/Components/Show_Components/Track.js
+++ src/Components/Show_Components/Track.js
@@ -2,13 +2,67 @@ import React from 'react';
 
 const Track = (props) => {
     let { track } = props
+    let trackLength = track.duration
+    const hr = 3600000
+    const min = 60000
+    const sec = 1000
+
+    function convertLength() {
+        var indexHr = 0, indexMin = 0, indexSec = 0
+
+        if (trackLength > hr) {
+            while (trackLength > hr) {
+                trackLength = trackLength - hr
+                indexHr++
+            } while (trackLength > min) {
+                trackLength = trackLength - min
+                indexMin++
+            } if (indexMin < 10) {
+                indexMin = "0" + indexMin
+            } while (trackLength > sec) {
+                trackLength = trackLength - sec
+                indexSec++
+            } if (indexSec < 10) {
+                indexSec = "0" + indexSec
+            } return (indexHr + ":" + indexMin + ":" + indexSec)
+        } else if (trackLength > min && indexHr == 0) {
+            while (trackLength > min) {
+                trackLength = trackLength - min
+                indexMin++
+            } while (trackLength > sec) {
+                trackLength = trackLength - sec
+                indexSec++
+            } if (indexSec < 10) {
+                indexSec = "0" + indexSec
+            } return (indexMin + ":" + indexSec)
+        } else if (trackLength > min && indexHr != 0) {
+            while (trackLength > min) {
+                trackLength = trackLength - min
+                indexMin++
+            } if (indexMin < 10) {
+                indexMin = "0" + indexMin
+            } while (trackLength > sec) {
+                trackLength = trackLength - sec
+                indexSec++
+            } if (indexSec < 10) {
+                indexSec = "0" + indexSec
+            } return (indexMin + ":" + indexSec)
+        } else {
+            while (trackLength > sec) {
+                trackLength = trackLength - sec
+                indexSec++
+            } if (indexSec < 10) {
+                indexSec = "0" + indexSec
+            } return (indexSec + "seconds")
+        }
+    }
 
     return (
         <div className="track-info">
             <div className="track-info-pos">{track.position}</div>
             <div className="track-info-name-length">
-            <div className="track-info-title">{track.title}</div>
-            <div className="track-info-length">{track.duration}</div>
+                <div className="track-info-title">{track.title}</div>
+                <div className="track-info-length">{convertLength(trackLength)}</div>
             </div>
         </div>
     );

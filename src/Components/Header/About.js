6a7123df330ba448d9f535e0f6274ba47ca4ba06
Caleb Tracey <caleb.tracey@gmail.com>
2021-01-03 15:03:33 -0500

    Text and styling for About section

--- src/Components/Header/About.js
+++ src/Components/Header/About.js
@@ -1,9 +1,26 @@
 const About = ({ shows }) => {
-    return (
-      <div>
-        <h2>About</h2>
+  return (
+    <div>
+      <h2>About</h2>
+      <div className="text-box">
+        This web application was created for learning and experimentation purposes.
+        </div>
+        <div className="text-box">
+        The Phish App provides a simple, user-friendly interface to navigate the
+        vast public archive of audience recordings of the band Phish. You can search for
+        a specific show by year, tour, or song. You may also provide a keyword into the 
+        search bar above. Each track of every show is also linked to an audio file and fully 
+        playable.
+      </div >
+      <div className="text-box">
+        The required data is gathered using an API provided by <a href="https://www.phish.in" target="_blank">Phish.in</a>.
+        Queries are done in read time based on the user's input. The resulting data is then used to provide an interface
+        to further refine your search until the desired show has been found. The app was built using 
+        the React library and utilizes the Node.js run-time environment. HTTP requests are done through Node.js using the 
+        Axios library. Styling comprises of CSS elements from the Bootstrap front-end framework.
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default About
+export default About

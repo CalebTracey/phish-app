3f188f17be23f690e4b84df5535f84ca0ee2cd40
Caleb Tracey <caleb.tracey@gmail.com>
2021-01-03 19:47:12 -0500

    Added placeholder content for Songs and Tours

---
+++ src/Containers/Songs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Songs = () => {
+    return (
+        <div className="songs-center">
+            <h1 className="songs-text">
+                Coming soon...
+            </h1>
+        </div>
+    );
+};
+
+export default Songs;
